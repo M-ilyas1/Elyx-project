888a079f1ac4e1af0d9e397fbcba4838d7b036c0
Muhammadabbas12345 <abbasarbab007@gmial.com>
2023-06-14 16:15:53 +0500

    add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Header from '../src/Component/Header/Header'
-import AuctionCard from './Component/AuctionCard/AuctionCard';
 import car from "./images/car.png";
 import ReviewCard from './Component/ReviewCard/ReviewCrad';
 
@@ -85,31 +84,10 @@ function App() {
         <Header />
       </header>
 
-      <section className="ml-5 mr-6" >
-        <h1 className="pb-5 font-semibold text-4xl font-[poppins]" >Latest Auctions</h1>
-          <hr className=" pb-7"/>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-9">
-            {
-              data.map((item) =>{
-                return(
-                  <AuctionCard
-                  price={item.price}
-                  name={item.name}
-                  model={item.model}
-                  engine={item.engine}
-                  location={item.location}
-                  image={item.image}
-                  />
-                )
-              })
-            }
-          </div>
-        </section>
-
       <section className="bg-[#F8F8FF] p-[50px] w-full">
       <div className="flex flex-wrap justify-center gap-7 ">
-  <ReviewCard />
-</div>
+     <ReviewCard /> 
+     </div>
 
         </section>
     </div>
