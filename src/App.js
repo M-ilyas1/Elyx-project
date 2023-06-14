9bc9a86f0ad0f434add9cc8e052997f9343e4960
rashuu-ali <rashidaliuiux@gmail.com>
2023-06-14 18:00:53 +0500

    completed footer

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import './App.css';
-import Header from '../src/Component/Header/Header'
+import "./App.css";
+import Header from "../src/Component/Header/Header";
 // import AuctionCard from './Component/AuctionCard/AuctionCard';
 // import car from "./images/car.png";
-import ReviewCard from './Component/ReviewCard/ReviewCrad';
-import Carousel from './Component/Carousel/Carousel';
+import ReviewCard from "./Component/ReviewCard/ReviewCrad";
+import Carousel from "./Component/Carousel/Carousel";
+import Example from "./Component/footer/footer";
 
 function App() {
-  
   return (
     <div className="App">
       <header>
         <Header />
       </header>
       <section className="bg-[#F8F8FF] p-[50px] w-full">
-      {/* <ReviewCard /> */}
+        {/* <ReviewCard /> */}
 
-      <div className=" bg-white  justify-center text-center align-middle w-full ">
+        <div className=" bg-white  justify-center text-center align-middle w-full ">
           <Carousel />
         </div>
-
-        </section>
+      </section>
 
       <section className="bg-[#F8F8FF] p-[50px] w-full">
-      <div className="flex flex-wrap justify-center gap-7 ">
-</div>
-
-        </section>
+        <div className="flex flex-wrap justify-center gap-7 "></div>
+      </section>
+      <section>
+        <div>
+         <footer/>
+        </div>
+      </section>
     </div>
   );
 }
