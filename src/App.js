4a070acc712ca4e0bc6eedc740ee6b4e2f007831
asifali12345678 <asifnagri55@gmail.com>
2023-06-14 15:40:47 +0500

    carousel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from '../src/Component/Header/Header'
 import ReviewCard from './Component/ReviewCard/ReviewCrad';
+import Carousel from './Component/Carousel/Carousel';
 
 function App() {
   return (
@@ -9,8 +10,10 @@ function App() {
         <Header />
       </header>
       <section className="bg-[#F8F8FF] p-[50px] w-full">
+      <ReviewCard />
           <div className="flex justify-center gap-7">
-            <ReviewCard />
+          <Carousel/> 
+           
           </div>
         </section>
     </div>
