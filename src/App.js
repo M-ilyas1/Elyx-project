13c454ae51cca0d33aecd8c7c3467679cd3bfb33
Muhammadabbas12345 <abbasarbab007@gmial.com>
2023-06-14 13:53:26 +0500

    compelete Review cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from '../src/Component/Header/Header'
+import ReviewCard from './Component/ReviewCard/ReviewCrad';
 
 function App() {
   return (
@@ -7,6 +8,11 @@ function App() {
       <header>
         <Header />
       </header>
+      <section className="bg-[#F8F8FF] p-[50px] w-full">
+          <div className="flex justify-center gap-7">
+            <ReviewCard />
+          </div>
+        </section>
     </div>
   );
 }
