0f8cff16ea25580b72de89f71e9113b75c3bef88
MuhammadEssa-514 <muhammadessa1512141@gmail.com>
2023-06-14 17:45:21 -0700

    Auction card

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       price: "$300000",
       name: "mitsubishi delica star wagon exceed",
       model: "1990",
-      engine: "7WD",
+      engine: "1WD",
       location: "California,USA",
       image: car,
     },
@@ -20,7 +20,7 @@ function App() {
       price: "$200000",
       name: "mitsubishi delica star wagon exceed",
       model: "2990",
-      engine: "7WD",
+      engine: "2WD",
       location: "California,USA",
       image: car,
     }, 
@@ -29,7 +29,7 @@ function App() {
       price: "$300000",
       name: "mitsubishi delica star wagon exceed",
       model: "3990",
-      engine: "7WD",
+      engine: "3WD",
       location: "Californsia,USA",
       image: car,
     },
@@ -38,31 +38,31 @@ function App() {
       price: "$400000",
       name: "mitsubishi delica star wagon exceed",
       model: "4990",
-      engine: "7WD",
+      engine: "4WD",
       location: "California,USA",
       image: car,
     },
     {
       id: 5,
-      price: "$500000",
+      price: "$5000000",
       name: "mitsubishi delica star wagon exceed",
       model: "5990",
-      engine: "7WD",
+      engine: "5WD",
       location: "California,USA",
       image: car,
     }, 
     {
       id: 6,
-      price: "$600000",
+      price: "$6000000",
       name: "mitsubishi delica star wagon exceed",
       model: "6990",
-      engine: "7WD",
+      engine: "6WD",
       location: "California,USA",
       image: car,
     },   
     {
       id: 7,
-      price: "$700000",
+      price: "$7000000",
       name: "mitsubishii delica star wagoon exceed",
       model: "7990",
       engine: "7WD",
@@ -74,7 +74,7 @@ function App() {
       price: "$800000",
       name: "mitsubishi delica star wagon exceed",
       model: "8990",
-      engine: "7WD",
+      engine: "8WD",
       location: "California,USA",
       image: car,
     }, 
@@ -87,7 +87,7 @@ function App() {
 
       <section className="w-full flex justify-center" >
         <div className="w-[1440px]">
-        <h1 className="pb-5 font-semibold text-4xl font-[poppins]" >Latest Auctions</h1>
+        <h1 className="pb-5 font-semibold text-4xl font-[poppins]" >Lattest Auctions</h1>
           <hr className=" pb-7"/>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-9">
             {
