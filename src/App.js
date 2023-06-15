90c52cd8a855006df5c0fc3c9b1bc29e230dae52
M-ilyas1 <mohammadilyas6800@gmail.com>
2023-06-15 16:53:28 +0500

    add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-function App() {
+import React from "react";
+import Header from "./Component/Header/Header";
 import Carousel from "./Component/Carousel/Carousel";
 import Example from "./Component/footer/footer";
 import car from "./images/car.png";
@@ -83,64 +84,54 @@ function App() {
   return (
     <section className="flex justify-center items-center">
       <div className="">
-      <header>
-        <Header />
-      </header>
-    
-      <div className=" bg-white  justify-center text-center align-middle w-full ">
-        <Carousel />
-      </div>
-    
-      <section className="w-full flex justify-center">
-        <div className="w-[1440px]">
-          <h1 className="pb-5 font-semibold text-4xl font-[poppins]">
-            Lattest Auctions
-          </h1>
-          <hr className=" pb-7" />
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-9">
-            {data.map((item) => {
-              return (
-                <AuctionCard
-                  price={item.price}
-                  name={item.name}
-                  model={item.model}
-                  engine={item.engine}
-                  location={item.location}
-                  image={item.image}
-                />
-              );
-            })}
+        <header>
+          <Header />
+        </header>
+
+        <section className="w-full flex justify-center">
+          <div className="w-[1440px]">
+            <h1 className="pb-5 font-semibold text-4xl font-[poppins]">
+              Lattest Auctions
+            </h1>
+            <hr className=" pb-7" />
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-9">
+              {data.map((item) => {
+                return (
+                  <AuctionCard
+                    price={item.price}
+                    name={item.name}
+                    model={item.model}
+                    engine={item.engine}
+                    location={item.location}
+                    image={item.image}
+                  />
+                );
+              })}
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-  <section className="bg-[#F8F8FF] p-[50px] w-full">
-  <div className=" bg-white  justify-center text-center align-middle w-full ">
-    <Carousel />
-  </div>
-</section>
+        <section className="bg-[#F8F8FF] p-[50px] w-full">
+          <div className=" bg-white  justify-center text-center align-middle w-full ">
+            <Carousel />
+          </div>
+        </section>
 
-      <section className="bg-[#F8F8FF] p-[50px] w-full">
-        <div className=" bg-white  justify-center text-center align-middle w-full ">
-          <Carousel />
-        </div>
-      </section>
 
-      <section>
-        <Product />
-      </section>
+        <section>
+          <Product />
+        </section>
 
-      <section className="bg-[#F8F8FF] p-[50px] w-full">
-        <div className="flex flex-wrap justify-center gap-7 "></div>
-      </section>
+        <section className="bg-[#F8F8FF] p-[50px] w-full">
+          <div className="flex flex-wrap justify-center gap-7 "></div>
+        </section>
 
-      <section>
-        <div>
-          <Example />
-        </div>
-      </section>
-
-    </div>
+        <section>
+          <div>
+            <Example />
+          </div>
+        </section>
+      </div>
     </section>
   );
 }
