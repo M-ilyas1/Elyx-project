9b160c29d6a68e70244d8be0179944b09a16fdea
M-ilyas1 <mohammadilyas6800@gmail.com>
2023-06-21 13:19:28 +0500

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,6 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-
-       
-
-        // 'md': '1355px',
-      
-
         'sm': '640px',
         // => @media (min-width: 640px) { ... }
   
