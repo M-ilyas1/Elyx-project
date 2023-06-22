29fd5529a27d1e1281e29dcce6a06ff143c5b75a
rashuu-ali <rashidaliuiux@gmail.com>
2023-06-22 17:43:09 +0500

    completed latestbids section

--- src/Component/ReviewCard/ReviewCrad.jsx
+++ src/Component/ReviewCard/ReviewCrad.jsx
@@ -61,8 +61,8 @@ function ReviewCard() {
         return (
           <div
             key={reviewData.id}
-            className="h-[350px] w-[336px] bg-white text-center pl-10 pr-10 pt-[55px] rounded-lg "
-          >
+            className="h-[350px] w-[336px] bg-white text-center pl-10 pr-10 pt-[55px] rounded-lg ">
+        
             <img
               src={reviewData.avatar}
               alt="Avatar"
