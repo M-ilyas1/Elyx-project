dd782ab57b258a7e20ae8368c0f9f728d357b7ea
Being-Murshid <murshid71704@gmail.com>
2023-06-23 04:41:58 -0700

    djfhak

--- src/Component/HeroSection/HeroSection.jsx
+++ src/Component/HeroSection/HeroSection.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Img from "../../image/img.png";
+import Img from "../../images/imgcar.png";
 import Button from "../Button/Button";
 import TimeBtn from "./TimeBtn";
 
@@ -10,7 +10,7 @@ function HeroSection() {
   const [timerSeconds, setTimerSeconds] = useState();
 
   const startTimer = () => {
-    const countDownDate = new Date("May 30,2024 ").getTime();
+    const countDownDate = new Date("july 30,2023").getTime();
 
     setInterval(() => {
       const now = new Date().getTime();
@@ -37,22 +37,22 @@ function HeroSection() {
   });
   return (
     <>
-      <div className="w-full h-[100vh] md:h-auto sm:h-auto flex relative ">
-        <div className="w-full 2xl:w-[70%] xl:w-[70%] lg:w-[70%] bg-[rgb(53,53,53)] flex md:flex-col-reverse sm:flex-col-reverse">
-          <div className="w-[705px] my-[118px] ml-[240px] lg:ml-[80px] xl:w-[550px] lg:w-[450px] md:my-[100px] md:ml-[100px] sm:ml-[50px] font-[Poppins] text-white">
-            <h1 className="text-[48px] lg:text-[30px] mb-2 lg:mb-0">
+      <div className="w-full h-auto flex relative ">
+        <div className="w-full 2xl:w-[70%] xl:w-[70%] lg:w-[70%] bg-[#353535] flex md:flex-col-reverse sm:flex-col-reverse xs:flex-col-reverse">
+          <div className="w-[705px] sm:w-[600px] xs:w-[310px] my-[118px] ml-[240px] lg:ml-[60px] xl:w-[550px] lg:w-[400px] md:my-[100px] sm:ml-[10px] xs:mx-auto font-[Poppins] text-white xs:mt-[18px] md:ml-[90px]">
+            <h1 className="text-[48px] lg:text-[30px] mb-2 lg:mb-0 xs:text-[28px]">
               2014 Mercedes-Benz E350 4MATIC Wagon
             </h1>
-            <p className="text-[20px] lg:text-[16px] leading-[30px] lg:leading-[25px] capitalize pr-3 mt-[24px] lg:mt-[10px]">
+            <p className="text-[20px] lg:text-[16px] leading-[30px] lg:leading-[25px] capitalize pr-3 mt-[24px] lg:mt-[10px] xs:mt-[14px]">
               This 2014 Porsche Carrera GT is one of 1,270 examples built during
               a four-year production run and was delivered...
             </p>
-            <div className="flex gap-20 my-1 uppercase mt-[48px] lg:mt-[20px]">
+            <div className="flex gap-20 xs:gap-5 my-1 uppercase mt-[48px] lg:mt-[20px] xs:flex-col xs:mt-[20px]">
               <div>
                 <h3 className="text-[20px] lg:text-[16px] leading-[18px]">
                   Current bid:
                 </h3>
-                <h1 className="text-[#0C50CA] text-[36px] lg:text-[22px] leading-[36px] mt-[6px]">
+                <h1 className="text-[#0C50CA] text-[36px] lg:text-[22px] xs:text-[30px] leading-[36px] mt-[6px]">
                   $215,000
                 </h1>
               </div>
@@ -60,12 +60,12 @@ function HeroSection() {
                 <h3 className="text-[20px] lg:text-[16px] leading-[18px]">
                   City :
                 </h3>
-                <h1 className="text-[#0C50CA] text-[36px] lg:text-[22px] leading-[36px] mt-[6px]">
+                <h1 className="text-[#0C50CA] text-[36px] lg:text-[22px] xs:text-[30px] leading-[36px] mt-[6px]">
                   Lexington, KY
                 </h1>
               </div>
             </div>
-            <div className="flex justify-between mt-[30px]">
+            <div className="flex justify-between mt-[30px] xs:mt-[20px]">
               <TimeBtn
                 timerDays={timerDays}
                 timerHours={timerHours}
@@ -73,7 +73,7 @@ function HeroSection() {
                 timerSeconds={timerSeconds}
               />
             </div>
-            <div className="mt-[48px] xl:mt-[35px] lg:mt-[25px] ">
+            <div className="mt-[48px] xl:mt-[35px] lg:mt-[25px] xs:mt-[20px]">
               <Button
                 label="PLACE BID"
                 height={"h-[72px]"}
@@ -82,16 +82,15 @@ function HeroSection() {
               />
             </div>
           </div>
-
-          <div className="2xl:absolute 2xl:right-[10%] 2xl:top-[8%] xl:absolute xl:right-[6%] xl:top-[7%] lg:absolute lg:right-[2%] lg:top-[12%] md:ml-[100px] md:mt-[50px] sm:mx-auto">
+          <div className="2xl:absolute 2xl:right-[15%] 2xl:top-[1%] xl:absolute xl:right-[5%] xl:top-[0%] lg:absolute lg:right-[4%] lg:top-[3%] md:ml-[100px] md:mt-[50px] sm:mx-auto xs:mx-auto border-black border-[20px] border-opacity-20">
             <img
               src={Img}
               alt=""
-              className="w-[640px] h-[640px] xl:h-[550px] xl:w-[550px] lg:w-[500px] lg:h-[500px]"
+              className="w-[640px] h-[640px] xl:h-[550px] xl:w-[550px] lg:w-[500px] lg:h-[500px] xs:w-[310px]  xs:h-[310px] xs:my-3"
             />
           </div>
         </div>
-        <div className="w-[30%] bg-[#0C50CA] hidden 2xl:block xl:block lg:block "></div>
+        <div className="w-[30%] bg-[#0C50CA] hidden 2xl:block xl:block lg:block"></div>
       </div>
     </>
   );
