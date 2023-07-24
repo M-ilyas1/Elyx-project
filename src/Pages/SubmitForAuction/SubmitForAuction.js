a070d0b8a7c70e1662c123d7fec7a4da9e179f40
M-ilyas1 <mohammadilyas6800@gmail.com>
2023-07-24 13:20:19 +0500

    create planpricing section

--- src/Pages/SubmitForAuction/SubmitForAuction.js
+++ src/Pages/SubmitForAuction/SubmitForAuction.js
@@ -1,21 +1,30 @@
-
-import React from 'react'
-import PlanMain from '../../Component/Plan/PlanMain'
-import PlaneProcess from '../../Component/PlanProcess/PlaneProcess'
-import { PopularItems } from '../../Component'
-import Footer from '../../Component/Footer/Footer'
+import React from "react";
+import PlanMain from "../../Component/Plan/PlanMain";
+import PlaneProcess from "../../Component/PlanProcess/PlaneProcess";
+import { PopularItems } from "../../Component";
+import Footer from "../../Component/Footer/Footer";
+import PlanPricing from "../../Component/PlanPricing/PlanPricing";
 
 export default function SubmitForAuction() {
   return (
     <div>
-      <PlanMain/>
-      <PlaneProcess/>
-      <PopularItems/>
-      <section>
-      <div>
-        <Footer bgColor="bg-[#FFFFFF]" textColor="text-[#353535]" />
+      <PlanMain />
+      <div className="container text-center mb-10">
+        <h1 className="text-[55px] font-bold mt-5">Your auction is just one click away</h1>
+        <p className="text-[21px] mb-20 text-gray-400 font-normal">
+          Our Application Process Is Free And Only Take 5 Minutes
+        </p>
+        <div>
+        <PlanPricing />
+        </div>
       </div>
+      <PlaneProcess />
+      <PopularItems />
+      <section>
+        <div>
+          <Footer bgColor="bg-[#FFFFFF]" textColor="text-[#353535]" />
+        </div>
       </section>
     </div>
-  )
-  }
+  );
+}
