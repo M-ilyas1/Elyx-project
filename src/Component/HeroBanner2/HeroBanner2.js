a631f84e723743eca856a01f3a03c16af299b863
M-ilyas1 <mohammadilyas6800@gmail.com>
2023-08-05 14:34:23 +0500

    resolve confilicts

--- src/Component/HeroBanner2/HeroBanner2.js
+++ src/Component/HeroBanner2/HeroBanner2.js
@@ -6,7 +6,7 @@ export default function HeroBanner2() {
     <div className='w-[100%]  bg-gray-100 grid grid-cols-1 sm:grid-cols-2'>
     <div className='h-[320px]'>
         <h1 className='text-[40px] font-bold pl-32 pt-20 leading-snug	'>Simply buy <span className='text-blue-800'>your Favoriate</span> <br/> product online</h1>
-        <p className='text-[18px] font-normal pl-32 pt-5 leading-4'>You order your car completely online.</p>
+        <p className='text-[18px] font-normal pl-32 pt-5 leading-4'>You Order your Car Completely Online.</p>
     </div>
     <div className='h-[320px]'>
         <img alt='MainImage.....' src={MainImg} />
